aa3f3a566aa28d91df54c177b5c2a9baf614be33
Bathula <SB065394@cerner.net>
2018-10-17 21:48:15 -0500

    adding new comment in child branch push

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,7 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+#creating the config xml and creating the domain using the domain utility
 
 include_recipe 'addmachine::commandfilexml'
 include_recipe 'addmachine::domainutility'
