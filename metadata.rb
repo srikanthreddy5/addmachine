22579c62d3fce5d35640286f0af64371e7e2f551
SB065394 <srikanthreddy.bathula@cerner.com>
2019-02-18 13:27:03 -0600

    edit metadata.db

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'addmachine'
-maintainer 'JitheshKumar (JK)'
-maintainer_email 'jiteshkumar.thekkeveettil@citi.com'
+maintainer ''
+maintainer_email '@citi.com'
 license 'all_rights'
 description 'Add machine to an RV based domain'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
